fc230402cdd0389a403441d54e85ec8877b9afaa
mounikaravi-hospet <mounikaravi.hospet@gmail.com>
2024-10-31 14:10:26 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import CatFacts from "./Components/CatFacts/CatFacts";
+import NyTimes from "./Components/NYTimes/NyTimes";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <CatFacts/> */}
+      <NyTimes />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/NYTimes/NyTimes.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+const NyTimes = () => {
+  const apikey = `Please put your own API key here`;
+
+  const [month, setMonth] = useState(1);
+  const [articles, setArticles] = useState(null);
+
+  const handleInput = (event) => {
+    setMonth(event.target.value);
+  };
+
+  const fetchArticles = (event) => {
+    if (event) {
+      event.preventDefault();
+    }
+    fetch(`/archive/v1/2024/${month}.json?api-key=${apikey}`)
+      .then((response) => {
+        if (!response.ok) throw new Error(`Error fetching data`);
+        return response.json();
+      })
+      .then((data) => {
+        setArticles(data.response.docs);
+      })
+      .catch((error) => {
+        console.error(error.message);
+      });
+  };
+
+  return (
+    <div>
+      <h1>NY Times Articles</h1>
+      <form>
+        <input type="text" name="month" id="month" onChange={handleInput} />
+        <button onClick={fetchArticles}>Search</button>
+      </form>
+
+      {articles && (
+        <div>
+          {articles.map((articles, index) => (
+            <div key={index}>
+              <h2>{articles.headline.main}</h2>
+              <p>{articles.abstrack}</p>
+              <p>{articles.source}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default NyTimes;
